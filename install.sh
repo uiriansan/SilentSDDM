98a491548d859f07a610214adcea58e250bee34c
uiriansan <uiriansan@gmail.com>
2025-06-24 17:25:49 -0300

    Improve clarity of the install script

--- install.sh
+++ install.sh
@@ -10,19 +10,19 @@ SHPATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 install_dependencies () {
     if command -v pacman &>/dev/null; then
-        echo -e "Installing dependencies with 'pacman'..."
+        echo -e "${grey}Installing dependencies with 'pacman'...${reset}"
         sudo pacman -S --needed sddm qt6-svg qt6-virtualkeyboard qt6-multimedia-ffmpeg
     elif command -v apt-get &>/dev/null; then
-        echo -e "Installing dependencies with 'apt'..."
+        echo -e "${grey}Installing dependencies with 'apt'...${reset}"
         sudo apt-get install sddm qt6-svg qt6-virtualkeyboard qt6-multimedia
     elif command -v xbps-install &>/dev/null; then
-        echo -e "Installing dependencies with 'xbps'..."
+        echo -e "${grey}Installing dependencies with 'xbps'...${reset}"
         sudo xbps-install sddm qt6-svg qt6-virtualkeyboard qt6-multimedia
     elif command -v dnf &>/dev/null; then
-        echo -e "Installing dependencies with 'dnf'..."
+        echo -e "${grey}Installing dependencies with 'dnf'...${reset}"
         sudo dnf install sddm qt6-qtsvg qt6-qtvirtualkeyboard qt6-qtmultimedia
     elif command -v zypper &>/dev/null; then
-        echo -e "Installing dependencies with 'zypper'..."
+        echo -e "${grey}Installing dependencies with 'zypper'...${reset}"
         sudo zypper install sddm-qt6 libQt6Svg6 qt6-virtualkeyboard qt6-virtualkeyboard-imports qt6-multimedia qt6-multimedia-imports
     else
         echo -e "\n${red}Could not install dependencies!\nDo it manually: ${cyan}https://github.com/uiriansan/SilentSDDM/wiki#dependencies${reset}\n"
@@ -31,18 +31,21 @@ install_dependencies () {
 }
 
 copy_files () {
+    echo -e "${grey}Copying files from '${SHPATH}/' to '/usr/share/sddm/themes/silent/'...${reset}"
     sudo mkdir -p /usr/share/sddm/themes/silent
     sudo cp -rf "$SHPATH"/. /usr/share/sddm/themes/silent/
 }
 
 copy_fonts () {
+    echo -e "${grey}Copying fonts to '/usr/share/fonts/'...${reset}"
     sudo cp -r /usr/share/sddm/themes/silent/fonts/{redhat,redhat-vf} /usr/share/fonts/
 }
 
 apply_theme () {
+    echo -e "${grey}Editing '/etc/sddm.conf'...${reset}"
     if [[ -f /etc/sddm.conf ]]; then
-        echo -e "${green}Creating backup for SDDM config in '/etc/sddm.conf.bkp'${reset}"
         sudo cp -f /etc/sddm.conf /etc/sddm.conf.bkp
+        echo -e "${green}Backup for SDDM config saved in '/etc/sddm.conf.bkp'${reset}"
 
         if grep -Pzq '\[Theme\]\nCurrent=' /etc/sddm.conf; then
             sudo sed -i '/^\[Theme\]$/{N;s/\(Current=\).*/\1silent/;}' /etc/sddm.conf
@@ -69,4 +72,4 @@ install_dependencies ;
 copy_files &&
 copy_fonts ;
 apply_theme &&
-echo -e "\n\n${green}Theme successfully installed!${reset}"
+echo -e "\n${green} Theme successfully installed!${reset}"
