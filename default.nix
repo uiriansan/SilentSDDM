205c2f57d23618fea31e8b155e7cbbfe688cb644
Rexiel Scarlet <37258415+Rexcrazy804@users.noreply.github.com>
2025-08-12 20:49:34 +0400

    flake: refactor code

--- default.nix
+++ default.nix
@@ -14,26 +14,33 @@
   withLayerShellQt ? true,
 }: let
   inherit (lib) cleanSource licenses;
+  inherit (lib) attrValues substring readFile concatStringsSep map;
+  inherit (lib.generators) toINI;
+  inherit (stdenvNoCC) mkDerivation;
 
-  theme-package = stdenvNoCC.mkDerivation (final: {
+  propagatedBuildInputs = attrValues {
+    inherit (kdePackages) qtmultimedia qtsvg qtvirtualkeyboard;
+  };
+
+  sddm-wrapped = kdePackages.sddm.override {
+    extraPackages = propagatedBuildInputs;
+    inherit withLayerShellQt withWayland;
+  };
+in
+  mkDerivation (final: {
+    inherit propagatedBuildInputs;
+
     pname = "silent";
-    version = "${builtins.substring 0 8 gitRev}";
-
+    version = "${substring 0 8 gitRev}";
     src = cleanSource ./.;
 
     dontWrapQtApps = true;
 
-    propagatedBuildInputs = with kdePackages; [
-      qtmultimedia
-      qtsvg
-      qtvirtualkeyboard
-    ];
-
     installPhase = let
       basePath = "$out/share/sddm/themes/${final.pname}";
       baseConfigFile = "${final.src}/configs/${theme}.conf";
-      overrides = lib.generators.toINI {} theme-overrides;
-      finalConfig = (builtins.readFile baseConfigFile) + "\n" + overrides;
+      overrides = toINI {} theme-overrides;
+      finalConfig = (readFile baseConfigFile) + "\n" + overrides;
       finalConfigFile = writeText "${theme}.conf" finalConfig;
     in ''
       mkdir -p ${basePath}
@@ -46,28 +53,20 @@
       cp ${finalConfigFile} ${basePath}/configs/${theme}.conf
 
       chmod -R +w ${basePath}/backgrounds
-      ${builtins.concatStringsSep "\n" (builtins.map (bg: "cp ${bg} ${basePath}/backgrounds/${bg.name}") extraBackgrounds)}
+      ${concatStringsSep "\n" (map (bg: "cp ${bg} ${basePath}/backgrounds/${bg.name}") extraBackgrounds)}
     '';
+
+    passthru.test = symlinkJoin {
+      name = "test-sddm-silent";
+      paths = [sddm-wrapped];
+      nativeBuildInputs = [makeWrapper];
+      postBuild = ''
+        makeWrapper $out/bin/sddm-greeter-qt6 $out/bin/test-sddm-silent \
+          --suffix QML2_IMPORT_PATH ':' ${final.finalPackage}/share/sddm/themes/${final.pname}/components \
+          --set QT_IM_MODULE qtvirtualkeyboard \
+          --add-flags '--test-mode --theme ${final.finalPackage}/share/sddm/themes/${final.pname}'
+      '';
+    };
 
     meta.licenses = licenses.gpl3;
-    passthru.test = test;
-  });
-
-  sddm-wrapped = kdePackages.sddm.override {
-    extraPackages = theme-package.propagatedBuildInputs;
-    inherit withLayerShellQt withWayland;
-  };
-
-  test = symlinkJoin {
-    name = "test-sddm-silent";
-    paths = [sddm-wrapped];
-    nativeBuildInputs = [makeWrapper];
-    postBuild = ''
-      makeWrapper $out/bin/sddm-greeter-qt6 $out/bin/test-sddm-silent \
-        --suffix QML2_IMPORT_PATH ':' ${theme-package}/share/sddm/themes/${theme-package.pname}/components \
-        --set QT_IM_MODULE qtvirtualkeyboard \
-        --add-flags '--test-mode --theme ${theme-package}/share/sddm/themes/${theme-package.pname}'
-    '';
-  };
-in
-  theme-package
+  })
