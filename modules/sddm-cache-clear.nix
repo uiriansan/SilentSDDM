38d0c7bcd8fee4b42bbbe4088d3c66847153f50e
0xDracula <abdallah.ebrahim.official@gmail.com>
2025-08-13 18:44:47 +0300

    update: disabled by default

--- modules/sddm-cache-clear.nix
+++ modules/sddm-cache-clear.nix
@@ -7,7 +7,7 @@ in
   options.sddm.cache = {
     clearOnBoot = lib.mkOption {
       type = lib.types.bool;
-      default = true;
+      default = false;
       description = ''
         Whether to clear the SDDM cache on every boot.
         This is useful for themes that sometimes fail to load due to stale cache.
@@ -18,7 +18,7 @@ in
   config = lib.mkIf cfg.clearOnBoot {
     services.displayManager.preStart = ''
       ${pkgs.util-linux}/bin/logger -t sddm-cache-clear "Clearing SDDM cache..."
-      ${pkgs.coreutils}/bin/rm -rf /var/lib/sddm/.cache
+      rm -rf /var/lib/sddm/.cache
       ${pkgs.util-linux}/bin/logger -t sddm-cache-clear "SDDM cache cleared."
     '';
   };
