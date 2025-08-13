6db637642e11fc3f6bf4cc9d0d91467d131c2196
0xDracula <abdallah.ebrahim.official@gmail.com>
2025-08-13 19:06:56 +0300

    fix: integrating it into the default.nix file, and removing the modules directory.

--- flake.nix
+++ flake.nix
@@ -17,7 +17,30 @@
           f (import nixpkgs {inherit system;})
       );
   in {
-    nixosModules.sddm-cache-clear = import ./modules/sddm-cache-clear.nix;
+    nixosModules.sddm-cache-clear = { config, lib, pkgs, ... }:
+    let
+      cfg = config.sddm.cache;
+    in
+    {
+      options.sddm.cache = {
+        clearOnBoot = lib.mkOption {
+          type = lib.types.bool;
+          default = false;
+          description = ''
+            Whether to clear the SDDM cache on every boot.
+            This is useful for themes that sometimes fail to load due to stale cache.
+          '';
+        };
+      };
+
+      config = lib.mkIf cfg.clearOnBoot {
+        services.displayManager.preStart = ''
+          ${pkgs.util-linux}/bin/logger -t sddm-cache-clear "Clearing SDDM cache..."
+          rm -rf /var/lib/sddm/.cache
+          ${pkgs.util-linux}/bin/logger -t sddm-cache-clear "SDDM cache cleared."
+        '';
+      };
+    };
     packages = forAllSystems (pkgs: rec {
       # you may test these themes with `nix run .#default.test`
       # similiarly `nix run .#example.test` will work too
